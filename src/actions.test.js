ea3d8e1f6fdaf6d962d1de241c9ce2c81d7129e3
mariajcb <bogomazm@gmail.com>
2020-05-05 16:55:37 -0400

    implements testing for actions

---
+++ src/actions.test.js
@@ -0,0 +1,35 @@
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED
+ } from './constants';
+
+import * as actions from './actions';
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+
+const mockStore = configureMockStore([thunkMiddleware])
+
+describe('actions', () => {
+  it('should create an action to search robots', () => {
+    const text = 'woooo';
+    const expectedAction = {
+      type: CHANGE_SEARCH_FIELD,
+      payload: text
+    }
+    expect(actions.setSearchField(text)).toEqual(expectedAction);
+  })
+})
+
+describe('fetch robots action PENDING', () => {
+  it('handles requesting robots API', () => {
+    const store = mockStore();
+    store.dispatch(actions.requestRobots());
+    const action = store.getActions();
+    const expectedAction = {
+      type: REQUEST_ROBOTS_PENDING
+    }
+    expect(action[0]).toEqual(expectedAction)
+  })
+})
